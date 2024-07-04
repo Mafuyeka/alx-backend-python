f00286871d462ad52d5a82acec4cdf47efe2fe7d
tshepisoroshina@gmail.com <tshepisoroshina@gmail.com>
2024-07-04 09:54:46 -0700

    Updated

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,19 +1,8 @@
-!/usr/bin/env python3
-"""
-This is a module that provides a function for finding the floor of a float.
-"""
+#!/usr/bin/env python3
+"""Module that contains function floor a float"""
 import math
 
 
 def floor(n: float) -> int:
-    """
-    This function finds the floor of a float and returns the result as an
-    integer.
-
-    Parameters:
-    n (float): The float to find the floor of.
-
-    Returns:
-    int: The floor of n.
-    """
+    """Function to floor a floating point value"""
     return math.floor(n)
